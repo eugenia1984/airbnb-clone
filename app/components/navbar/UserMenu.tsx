570a5fbc9cbe07d738eafbd5b8f938a84a371f27
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-08-27 22:47:33 -0300

    -create de login

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,27 +4,36 @@ import { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
+
 import useRegisterModal from '@/app/hooks/useRegisterModal'
+import useLoginModal from '../../hooks/useLoginModal'
 
 const UserMenu = () => {
   const registerModal = useRegisterModal()
+  const loginModal = useLoginModal()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value)
   }, [])
 
+  const airbnbBtn = "hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+
+  const menuBtn = "absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white oflerflow-hidden right-0 top-12 text-sm"
+
+  const avatarBtn = "p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+
   return (
     <div className="relative">
       <div className="flex flex-center items-center gap-3">
         <div
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+          className={ airbnbBtn }
           onClick={ () => { } }
         >
           Airbnb your home
         </div>
         <div
-          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          className={ avatarBtn }
           onClick={ toggleOpen }
         >
           <AiOutlineMenu />
@@ -34,11 +43,11 @@ const UserMenu = () => {
         </div>
       </div>
       { isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white oflerflow-hidden right-0 top-12 text-sm">
+        <div className={ menuBtn }>
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem
-                onClick={ () => { } }
+                onClick={ loginModal.onOpen }
                 label="Login"
               />
               <MenuItem
