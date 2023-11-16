c83354183023c841732e0c4ca4761c42d6700f53
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-11-16 10:55:12 -0300

    -add the title and the favicon

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -22,11 +22,11 @@ const MarketingPage = () => {
             headingFont.className
           ) }
       >
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full text-amber-700 uppercase">
+        <div className="mb-6 flex items-center border shadow-sm p-4  bg-amber-100 text-amber-700 rounded-full text-amber-700 uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task managment
         </div>
-        <h1 className="text-3xl md:text-5xl txt-center text-neutral-800 mb-6">
+        <h1 className="text-3xl md:text-5xl text-center text-neutral-800 mb-6 px-4">
           Taskify helps team move
         </h1>
         <div className="text-3l md:text-5xl bg-gradient-to-r from-fuchsia-800 to-pink-500 text-white px-4 py-2 rounded-md mb-4 w-fit">
@@ -34,7 +34,7 @@ const MarketingPage = () => {
         </div>
         <p
           className={
-            cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md_max-w-2xl text-center mx-auto",
+            cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto px-4",
               textFont.className
             ) }
         >
