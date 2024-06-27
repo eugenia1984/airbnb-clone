7e785671c59cb7cc007ddcdf31bed062e103010c
eugenia1984 <costamariaeugenia1@gmail.com>
2024-06-27 18:42:24 +0200

    feat: add shadcn-ui components for forms

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,7 +1,18 @@
 export default function DescriptionPage () {
   return(
-    <div>
-      Hello from the description page you have added a category
-    </div>
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          Please describe your home
+        </h2>
+      </div>
+      <form>
+        <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-2">
+            
+          </div>
+        </div>
+      </form>
+    </>
   )
 }
