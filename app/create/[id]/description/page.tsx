86821515f7291dc4f5bc347b4d4864569b6056f5
eugenia1984 <costamariaeugenia1@gmail.com>
2024-06-27 18:55:09 +0200

    feat :start with the description page

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,3 +1,8 @@
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Card, CardHeader } from "@/components/ui/card"
+
 export default function DescriptionPage () {
   return(
     <>
@@ -9,8 +14,54 @@ export default function DescriptionPage () {
       <form>
         <div className="mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36">
           <div className="flex flex-col gap-y-2">
-            
+            <Label>Title</Label>
+            <Input 
+              name="title" 
+              required 
+              placeholder="Please add the title here ..."
+              type="text"
+            />
           </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Description</Label>
+            <Textarea 
+              name="description"
+              required
+              placeholder="Please add the description here..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Price</Label>
+            <Input 
+              name="price" 
+              required 
+              type="number"
+              placeholder="Price per night in EUR ..."
+              min={10}
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Image</Label>
+            <Input 
+              name="image" 
+              required 
+              type="file"
+            />
+          </div>
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">
+                    Guest
+                  </h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many guest ?
+                  </p>
+                </div>
+              </div>
+            </CardHeader>
+          </Card>
         </div>
       </form>
     </>
