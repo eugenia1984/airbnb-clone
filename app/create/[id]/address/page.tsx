bde7924b09b08487dcba02404ac0537e23b78513
eugenia1984 <costamariaeugenia1@gmail.com>
2024-06-28 16:56:37 +0200

    feat: add the server action createLocation

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,6 +1,10 @@
 
 "use client"
 
+import { useState } from "react"
+
+import dynamic from "next/dynamic"
+
 import { useCountries } from "@/app/lib/getCountries"
 
 import { HeadlineH2 } from "@/app/components/ui/HeadlineH2"
@@ -14,9 +18,19 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select"
+import { Skeleton } from "@/components/ui/skeleton"
+import { CreationButton } from "@/app/components/CreationButton"
 
 export default function AddressRoute() {
   const { getAllCountries } = useCountries()
+  const [locationValue, setLocationValue] = useState('')
+
+  const LazyMap = dynamic(() => import('@/app/components/Map'), {
+    ssr: false,
+    loading: () =>  <Skeleton className="h-[50vh] w-full" />
+  })
+
+  const sortedCountries = getAllCountries().sort((a, b) => a.label.localeCompare(b.label))
 
   return (
     <>
@@ -24,16 +38,16 @@ export default function AddressRoute() {
         <HeadlineH2 text="Where is your home located?" />
       </section>
       <form>
-        <div className="w-3/5 mx-auto">
+        <div className="w-3/5 mx-auto mb-36">
           <div className="mb-5">
-            <Select required>
+            <Select required onValueChange={(value) => setLocationValue(value) }>
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="Select a country" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Countries</SelectLabel>
-                  {getAllCountries().map((country) => (
+                   {sortedCountries.map((country) => (
                     <SelectItem key={country.value} value={country.value}>
                       {country.flag} - {country.label} / {country.region}
                     </SelectItem>
@@ -42,7 +56,9 @@ export default function AddressRoute() {
               </SelectContent>
             </Select>
           </div>
+          <LazyMap locationValue={locationValue} />
         </div>
+        <CreationButton />
       </form>
     </>
   )
